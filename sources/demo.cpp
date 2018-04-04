9a34c171c1769fcbfa2138333b3de7134d608dec
Elijah Doman <gshkvbmstu@gmail.com>
2018-04-04 18:33:12 +0300

    Update demo.cpp

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -3,7 +3,8 @@
 
 int main(int argc, char** argv) {
 std::string text;
-std::string log_path = std::getenv("LOH_PATH");
+std::string log_path = std::getenv("LOG_PATH");
+std::ofstream out(log_path, std::ios_base::app);
 while(std::cin >> text) {
 print(text, out);
 out << std::endl;
